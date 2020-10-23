df19fbdc3a6315e78c331332cf2893cd4fc806e7
Mateusz <mateusz.splawski96@gmail.com>
2020-10-23 23:42:54 +0200

    adding composer

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!-- bootstraping cms -->
+<?php require("./vendor/autoload.php"); ?>
+
 <?php
 @require_once("./themes/mainTheme/head.php");
 @require_once("./themes/mainTheme/header.php");
--- themes/mainTheme/page.php
+++ themes/mainTheme/page.php
@@ -1,5 +1,6 @@
 <body>
 <main class="page">
 This is page
+<button class="btn primary">Hello</button>
 </main>
 </body>

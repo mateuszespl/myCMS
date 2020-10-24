5f411be2ac2adb319a9f6866ae05293717043a3b
Mateusz <mateusz.splawski96@gmail.com>
2020-10-24 16:26:39 +0200

    client routing setup

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!-- bootstraping cms -->
 
 <?php
+@require_once("./config/consoleLog.php");
+// Routing 
+
+switch($_SERVER["REQUEST_URI"]){
+    case "/":
+        echo "HEllo";
+    case "/admin":
+        @include_once("./admin/index.php");
+};
 @require_once("./themes/lightTheme/head.php");
 @require_once("./themes/lightTheme/header.php");
 @require_once("./themes/lightTheme/page.php");

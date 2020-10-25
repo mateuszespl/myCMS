6f9160bb5e3e670591afe11e6f3abe7e4bae4871
Mateusz <mateusz.splawski96@gmail.com>
2020-10-25 14:17:13 +0100

    connecting to db

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php 
 @include_once 'head.php';
 @include_once 'navigation.php';
+@include_once 'loginForm.php';
  ?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+@include_once('./config/consoleLog.php');
+@include_once('./config/db.php');
+
 // Routing 
 switch($_SERVER["REQUEST_URI"]){
     case "/":

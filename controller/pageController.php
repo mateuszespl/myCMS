958a5052203f8e54b2cae819ada033b2f0777109
Mateusz <mateusz.splawski96@gmail.com>
2020-11-16 23:13:02 +0100

    page controller update

--- controller/pageController.php
+++ controller/pageController.php
@@ -1,6 +1,12 @@
 <?php 
 class PageController{
     function displayPage($slug){
+        global $pages;
+        foreach($pages as $page){
+            if($slug == $page[8]){
+                header("Location:/?p=page&id=".$page[0]);
+            }
+        }
         @include_once 'view/page/navigation.php';
         @include_once 'view/page/page.php';
     }
